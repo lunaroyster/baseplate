400369e323b8e58dae6a42db8829181abaf47f1b
lunaroyster <oneinaquadrillion@live.com>
2025-12-17 14:07:37 +0530

    upgraded tailwindcss

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#f6fff1] to-[#f2dbba] text-black">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-linear-to-b from-[#f6fff1] to-[#f2dbba] text-black">
         <h1 className="text-4xl font-semibold">Baseplate.</h1>
       </main>
     </HydrateClient>
